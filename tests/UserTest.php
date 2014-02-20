94168a1ff5c35adfbc5bcd272f0eb76fd1233d62
jordi quimaso alarcon <jquimaso@gmail.com>
2014-02-20 22:15:51 +0100

    no message

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -7,6 +7,23 @@ class UserTest extends PHPUnit_Framework_TestCase
 
 
 
+    /**
+     * @param $user_data
+     * @return bool
+     */
+    public function testvalidateData(){
+       /* $myUsers = new \Development\User();
+        $myUsers->newUser();
+
+        //var_dump($array);
+        if (count($array) > 0) {
+            $this->assertEquals(true,true);
+        }
+        else{
+            $this->setExpectedException('InvalidArgumentException');
+        }*/
+    }
+
 
 
 
